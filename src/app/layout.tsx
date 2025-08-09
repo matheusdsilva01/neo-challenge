514b1a09195fbccdc51a25c7c078217b3d634344
Matheus Silva <ms25022003@gmail.com>
2025-08-09 10:49:58 -0300

    feat(metadata): add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,12 @@ import StyledComponentsRegistry from '@/lib/styled-components/registry';
 import ClientLayout from '@/lib/styled-components/client-layout';
 import { StoreProvider } from './StoreProvider';
 import { Header } from '@/components/components/Header';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Marvel Comics',
+  description: 'Desafio Técnico utilizando API da Marvel',
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
